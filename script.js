9565db242abce81f7345856d0065ca5ee32aca58
Junninho Thomas <jthomas2016@bklawtech.com>
2015-11-18 22:03:53 +0000

    Slack

--- script.js
+++ script.js
@@ -17,10 +17,15 @@ $('#submit-message').submit(function (e) {
   e.preventDefault();
 
   /* TODO: send message to server */
-
+  var username = $('#submit-user').val();
+  var content = $('#submit-content').val();
+  
+  sendMessage(username, content);
 });
 
-
+function formatMessage(message) {
+  return '<li>' + '<span class="username">' + message.user + '</span>' + ': ' + message.content + '</li>';
+}
 /* use setInterval to periodically get new messages and update the list */
 
 window.setInterval(function () {
@@ -30,6 +35,11 @@ window.setInterval(function () {
     $('.messages').empty();
 
     // TODO: append messages to <ul class="messages">
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      // var text = message.user + ': ' + message.content;
+      $('.messages').append(formatMessage(message));
+    }
   });
 
 }, 300);
