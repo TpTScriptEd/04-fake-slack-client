00bce6d4e904580de2324ba5ab96b271cb12ee53
jacrispy <cavila@aoiths.org>
2015-11-18 22:07:54 +0000

    si

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var host = 'https://node-slack-christopherjkim.c9.io';
 var sendRoute = host + '/send';
 var messagesRoute = host + '/messages';
 
-function sendMessage (user, content, callback) {
-  $.post(sendRoute, {user: user, content: content})
-  .done(callback);
+function sendMessage (user, content, callback) 
+{
+  $.post(sendRoute, {user: user, content: content}).done(callback);
 }
 
-function getMessages (callback) {
-  $.get(messagesRoute)
-  .done(callback);
+function getMessages (callback) 
+{
+  $.get(messagesRoute).done(callback);
 }
 
 
@@ -17,19 +17,33 @@ $('#submit-message').submit(function (e) {
   e.preventDefault();
 
   /* TODO: send message to server */
-
+  var username = $('#submit-user').val();
+  var content = $('#submit-content').val();
+  
+  console.log(username, content);
+  
+  sendMessage(username, content);
 });
 
 
 /* use setInterval to periodically get new messages and update the list */
 
-window.setInterval(function () {
+window.setInterval(function () 
+{
 
-  getMessages(function (messages) {
+  getMessages(function (messages) 
+  {
     // empty message log
     $('.messages').empty();
 
     // TODO: append messages to <ul class="messages">
+    for(var i = 0; i < messages.length; i++)
+    {
+      var messages = messages[i]; 
+      //$('.messages').append(JSON.stringify(messages));
+      var text = message.user + ': ' + message.content;
+      $('.messages').append('<li>' + text + '<li>');
+    }
   });
 
 }, 300);
